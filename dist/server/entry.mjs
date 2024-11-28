df1eca4ed8d7fa4e117c529867328276f613b773
PepperHorn <info@pepperhorn.com>
2024-11-29 07:01:15 +1100

    added dist to commit

---
+++ dist/server/entry.mjs
@@ -0,0 +1,36 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BjBC8U-5.mjs';
+import { manifest } from './manifest_UjZxU9Z7.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/node.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+const serverIslandMap = new Map();
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "mode": "standalone",
+    "client": "file:///home/ph/creativerangeslanding/astro-landing-page/dist/client/",
+    "server": "file:///home/ph/creativerangeslanding/astro-landing-page/dist/server/",
+    "host": false,
+    "port": 4321,
+    "assets": "_astro"
+};
+const _exports = createExports(_manifest, _args);
+const handler = _exports['handler'];
+const startServer = _exports['startServer'];
+const options = _exports['options'];
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { handler, options, pageMap, startServer };
